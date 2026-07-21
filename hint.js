c3d8926fd86d017bb3f8873f2226d43c2861d9e2
Mohas Kiani <mohammadhasankiani2007@gmail.com>
2026-07-21 19:54:18 +0330

    hint

---
+++ hint.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const hintBox = document.getElementById("hint-message");
+
+    setTimeout(function() {
+        hintBox.style.opacity = "0";
+    }, 4000); 
+
+});
