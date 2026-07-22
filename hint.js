96581d5e8faeeec5ac34acccce9c014ce311e9e8
Mohas Kiani <mohammadhasankiani2007@gmail.com>
2026-07-22 12:26:10 +0330

    tarjome

--- hint.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const hintBox = document.getElementById("hint-message");
-
-    setTimeout(function() {
-        hintBox.style.opacity = "0";
-    }, 4000); 
-
-});
--- sc.js
+++ sc.js
@@ -10,3 +10,16 @@ sentences.forEach(sentence => {
   });
   
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    const hintBox = document.getElementById("hint-message");
+
+    setTimeout(function() {
+        hintBox.style.opacity = "1";
+        setTimeout(function(){
+          hintBox.style.opacity = "0";
+        },4000);
+    
+      },2000); 
+});
